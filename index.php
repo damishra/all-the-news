22a2f0896507ae080bb08b8f672542392f636ebc
root <root@dishantmishra.me>
2019-03-21 09:22:53 +0000

    :D

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="./assets/styles/bulma.css">
+    <script defer src="./assets/scripts/all.js"></script>
+</head>
+<body>
 <?php
-    require 'classes/mongodb.class.php';
-
-    $db = new MongoDB();
-
-
-
-
+require 'classes/DB.class.php';
+try {
+    $db = new DB();
+    $db->fetchArticles();
+} catch (Exception $e) {
+    die ('???');
+}
 ?>
+</body>
+</html>
+
