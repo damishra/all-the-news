d002adb0dae9c21ec6f772a3c3aaaf05ea4a6bff
root <root@dishantmishra.me>
2019-03-24 17:22:07 +0000

    emergency push

--- index.php
+++ index.php
@@ -1,22 +1,58 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <title>Hello Bulma!</title>
     <link rel="stylesheet" href="./assets/styles/bulma.css">
+    <link rel="stylesheet" href="./assets/styles/default.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+128+Text" rel="stylesheet">
     <script defer src="./assets/scripts/all.js"></script>
 </head>
 <body>
-<?php
-require 'classes/DB.class.php';
-try {
-    $db = new DB();
-    $db->fetchArticles();
-} catch (Exception $e) {
-    die ('???');
-}
-?>
+    <div class="container is-fullhd">
+        <br>
+        <div class="columns">
+            <div class="column is-one-fifth">
+                <div class="box is-boxed" style="position: fixed">
+                    <a class="navbar-content" href="https://dishantmishra.me/all-the-news" style="font-family: 'Libre Barcode 128 Text', 'Helvetica Neue', Helvetica, Arial, serif; background-color: #222;">
+                        <h1 class="subtitle is-1" style=" color: #ddd; padding: 5px;">All The News!</h1>
+                    </a>
+                    <br>
+                    <hr>
+                    <br>
+                    <form method="post" action="./assets/scripts/formAction.php">
+                        <div class="field">
+                            <label class="label" style="color: #eee;">SEARCH</label>
+                            <div class="control">
+                                <p class="control has-icons-left has-icons-right">
+                                    <input class="input is-small is" type="text" placeholder="Search Text" name="term">
+                                    <span class="icon is-small is-left">
+                                  <i class="fas fa-search"></i>
+                                </span>
+                                </p>
+                                <div style="min-height: 10px"></div>
+                                <p>
+                                    <input type="submit" class="button is-small is-dark" value="search">
+                                </p>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="allowScroll column is-four-fifths">
+                <?php
+                require 'classes/DB.class.php';
+                try {
+                    $db = new DB();
+                    $db->fetchArticles();
+                } catch (Exception $e) {
+                    die ('???');
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
